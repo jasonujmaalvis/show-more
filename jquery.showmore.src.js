efa47f6e7069857fab331cb2b8ca3d78f41de167
Jason Alvis <alvis.jason@gmail.com>
2013-05-21 21:43:19 +0200

    Update jquery.showmore.src.js

--- jquery.showmore.src.js
+++ jquery.showmore.src.js
@@ -3,7 +3,7 @@
  * Author: Jason Alvis
  * Author Site: http://www.jasonalvis.com
  * License: Free General Public License (GPL)
- * Version: 1.0
+ * Version: 1.0.0
  * Date: 14.06.2012
  */
 (function($) {
